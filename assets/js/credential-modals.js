259f375a1e1db326013ceefe08b350f680b0de39
jdblundell-dc <77901359+jdblundell-dc@users.noreply.github.com>
2025-01-17 20:44:50 -0600

    acalog-testing
    BOOM BOOM

---
+++ assets/js/credential-modals.js
@@ -0,0 +1,42 @@
+    $(document).ready(function () {
+        console.log('Document is ready');
+        $('.acalog').acalogWidgetize({
+            gateway: 'https://catalog.dallascollege.edu',
+        });
+
+        setTimeout(function () {
+            $('#jq_resultsContainer').find('.acalog-close').remove();
+        }, 10000);
+        console.log('Acalog loads');
+
+        // Event listener for links
+        $('a.credential').on('click', function (event) {
+            event.preventDefault();
+            console.log('Link clicked');
+            var programId = $(this).attr('cat-id');
+            var ed2go = $(this).attr('ed2go');
+            var gpsId = $(this).attr('gps-id');
+
+            console.log('programId:', programId);
+            console.log('ed2go:', ed2go);
+            console.log('gpsId:', gpsId);
+
+            if(programId) {
+                $('#CredentialModal .modal-body').html(
+                    '<div class="acalog" data-acalog-data="programs" data-acalog-catalog-legacy-id="2" data-acalog-program-id="' + programId + '">Loading...</div>');
+                    // Re-initialize acalogWidgetize for the new content
+                    $('.acalog').acalogWidgetize({
+                        gateway: 'https://catalog.dallascollege.edu',
+                    });
+
+            $('#CredentialModal').modal('show');
+            } else if (ed2go === 'yes') {
+                $('#CredentialModal .modal-body').html(
+                    '<div>Ed2Go is a partnership with Dallas College offering online CE. You can find certificates at <a href="https://www.ed2go.com">ed2go.com</a></div>');
+            } else if (gpsId) {
+                $('#CredentialModal .modal-body').html(
+                    '<div><a href="https://www.dallascollege.edu/cd/gps/pages/gpsmap.aspx?gpsId=' + gpsId + '">View the Guided Pathway</a></div>');
+            }
+
+        });
+    });
