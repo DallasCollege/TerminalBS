3329306172e5c28ca45e3d86eaddf8bfc0a3bd39
rscottdc <rscott@dcccd.edu>
2025-01-24 18:02:44 -0600

    Merged modal and pop-sandbox JS files

--- assets/js/credential-modals.js
+++ assets/js/credential-modals.js
@@ -49,6 +49,64 @@
     });
 
     // RFI form on Area of Study pages
+    document.addEventListener('DOMContentLoaded', function () {
+      document.getElementById('tfa_14').addEventListener('change', toggle_CE_related);
+      document.getElementById('tfa_66').addEventListener('change', toggle_PLA_related);
+      document.getElementById('tfa_67').addEventListener('change', toggle_PLA_related);
+
+      // Trigger modal on button click
+      document.getElementById('RMIbutton').addEventListener('click', function () {
+        var formnode = document.getElementById("formbox");
+        formnode.classList.remove("extra-padding-2");
+        var formcontainer = document.getElementById("popformcontainer");
+        var formclone = formnode.cloneNode(true);
+        var modalnode = document.getElementById("modal-form-content");
+
+        //clear content of modal node
+        while (modalnode.firstChild) {
+          modalnode.removeChild(modalnode.firstChild);
+        }
+        //insert clone into modal
+        modalnode.appendChild(formclone);
+        //clear content of form node
+        while (formcontainer.firstChild) {
+          formcontainer.removeChild(formcontainer.firstChild);
+        }
+        // re-add event listeners to form fields
+        document.getElementById('tfa_14').addEventListener('change', toggle_CE_related);
+        document.getElementById('tfa_66').addEventListener('change', toggle_PLA_related);
+        document.getElementById('tfa_67').addEventListener('change', toggle_PLA_related);
+      });
+
+      //listen for when modal is closed
+      var myModal = document.getElementById('RMIModal');
+      myModal.addEventListener('hidden.bs.modal', function (event) {
+        var modalnode = document.getElementById("formbox");
+        modalnode.classList.add("extra-padding-2");
+        var modalclone = modalnode.cloneNode(true);
+        var formnode = document.getElementById("popformcontainer");
+        var modalContent = document.getElementById('modal-form-content');
+        //console.log(modalclone);
+
+        //clear content of modal form node
+        modalContent.replaceChildren();
+
+        // clear bottom form area
+        while (formnode.firstChild) {
+          formnode.removeChild(formnode.firstChild);
+        }
+        //insert clone into form area
+        formnode.appendChild(modalclone);
+
+        // re-add event listeners to form fields
+        document.getElementById('tfa_14').addEventListener('change', toggle_CE_related);
+        document.getElementById('tfa_66').addEventListener('change', toggle_PLA_related);
+        document.getElementById('tfa_67').addEventListener('change', toggle_PLA_related);
+      });
+
+    });
+    // End RFI form on Area of Study pages
+
 
     // look for CE related fields
     function toggle_CE_related() {
@@ -73,10 +131,3 @@
             targetElement.style.display = 'none';
         }
     }
-
-    document.getElementById('tfa_14').addEventListener('change', toggle_CE_related);
-    document.getElementById('tfa_66').addEventListener('change', toggle_PLA_related);
-    document.getElementById('tfa_67').addEventListener('change', toggle_PLA_related);
-
-
-    // End RFI form on Area of Study pages
